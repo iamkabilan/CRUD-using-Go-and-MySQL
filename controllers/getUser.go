3e950b29449b8dcf634cd7b1940329345f7d7148
Kabilan <kabilan185@gmail.com>
2024-09-23 10:52:08 +0530

    Implemented redis in get user endpoint

--- controllers/getUser.go
+++ controllers/getUser.go
@@ -1,13 +1,26 @@
 package controllers
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/iamkabilan/CRUD-using-Go-and-MYSQL/database"
 )
+
+func findUser(users []User, id int) *User {
+	for _, user := range users {
+		if user.Id == id {
+			return &user
+		}
+	}
+
+	return nil
+}
 
 func GetUser(responseWriter http.ResponseWriter, request *http.Request) {
 	variables := mux.Vars(request)
@@ -16,28 +29,49 @@ func GetUser(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.Header().Set("Content-Type", "application/json")
 
 	db := database.GetDB()
+	redisConn := database.GetRedis()
+	ctx := context.Background()
 
-	query := "SELECT id, name, email FROM users WHERE id = ?"
-	row := db.QueryRow(query, userId)
+	var users []User
+	result := database.GetUsers(redisConn, "users", ctx)
 
-	var user User
-	err := row.Scan(&user.Id, &user.Name, &user.Email)
+	for _, userJson := range result {
+		var user User
+		json.Unmarshal([]byte(userJson), &user)
+		users = append(users, user)
+	}
 
-	if err != nil {
-		if err == sql.ErrNoRows {
-			responseWriter.WriteHeader(http.StatusNotFound)
+	id, _ := strconv.Atoi(userId)
+	userExist := findUser(users, int(id))
+
+	if userExist != nil {
+		json.NewEncoder(responseWriter).Encode(userExist)
+		log.Println("User exist in Redis....")
+	} else {
+		log.Println("User does not exist in Redis....")
+
+		query := "SELECT id, name, email FROM users WHERE id = ?"
+		row := db.QueryRow(query, userId)
+
+		var user User
+		err := row.Scan(&user.Id, &user.Name, &user.Email)
+
+		if err != nil {
+			if err == sql.ErrNoRows {
+				responseWriter.WriteHeader(http.StatusNotFound)
+				json.NewEncoder(responseWriter).Encode(map[string]string{
+					"error": "User not found",
+				})
+				return
+			}
+
+			responseWriter.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(responseWriter).Encode(map[string]string{
-				"error": "User not found",
+				"error": "Internal server error",
 			})
 			return
 		}
 
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(responseWriter).Encode(map[string]string{
-			"error": "Internal server error",
-		})
-		return
+		json.NewEncoder(responseWriter).Encode(user)
 	}
-
-	json.NewEncoder(responseWriter).Encode(user)
 }
